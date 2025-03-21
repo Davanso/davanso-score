af00c445becc35488abd853f79fc02fb860c542b
davanso <guipvp234@gmail.com>
2025-03-21 10:31:34 -0300

    feat ✨: change footer style and add icons

--- src/components/footer_component/footer.jsx
+++ src/components/footer_component/footer.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import './Footer.css'; // Import the CSS file
+import { FaFacebook, FaTimes, FaInstagram, FaTiktok } from 'react-icons/fa';
+import './footer.css';
+import {FaXTwitter} from "react-icons/fa6";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footerContent">
-                <h1 className="title">DAVANSO SCORE</h1>
-                <div className="links">
-                    <a href="/" className="link">Home</a>
-                    <a href="/about" className="link">About</a>
-                    <a href="/contact" className="link">Contact</a>
+        <footer>
+            <div className="top-section">
+                <h1 className="logo">DavansoScore!</h1>
+            </div>
+
+            <div className="middle-section">
+                <nav>
+                    <a href="#">PUBLICAR</a>
+                    <a href="#">CONTATO</a>
+                    <a href="#">EDITOR</a>
+                    <a href="#">NEWS</a>
+                </nav>
+                <div className="social-icons">
+                    <a href="#"><FaXTwitter /></a>
+                    <a href="#"><FaInstagram /></a>
+                    <a href="#"><FaTiktok /></a>
                 </div>
+            </div>
+
+            <div className="bottom-section">
+                <p>© 2025 DavansoScore! – Todos os direitos reservados.</p>
             </div>
         </footer>
     );
