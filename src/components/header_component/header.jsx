7a6bd9d5e33316d800c833903107a00257fa387e
davanso <guipvp234@gmail.com>
2025-03-21 10:19:13 -0300

    feat ✨: change header style and add icons

--- src/components/header_component/header.jsx
+++ src/components/header_component/header.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { FaUser, FaBell, FaCog } from 'react-icons/fa'; // Import icons
-import './header.css'; // Import the CSS file
+import { FaUserCircle, FaStar, FaQuestionCircle, FaCog } from 'react-icons/fa';
+import { IoSearch } from 'react-icons/io5';
+import { BiFootball, BiBasketball, BiTennisBall } from 'react-icons/bi';
+import {GiBoxingGlove, GiFullMotorcycleHelmet, GiVolleyballBall} from 'react-icons/gi';
+import './header.css';
+import {CgProfile} from "react-icons/cg";
 
 const Header = () => {
     return (
-        <header className="header">
-            <h1 className="title">DAVANSO SCORE</h1>
-            <div className="icons">
-                <FaBell className="icon" />
-                <FaCog className="icon" />
-                <FaUser className="icon" />
+        <header>
+            <div className="left-section">
+                <h1 className="logo">Davanso Score</h1>
+                <nav>
+                    <button><BiFootball /> <span>Futebol</span></button>
+                    <button><BiBasketball /> <span>Basquete</span></button>
+                    <button><GiVolleyballBall /> <span>Vôlei</span></button>
+                    <button><GiBoxingGlove /> <span>MMA</span></button>
+                    <button><BiTennisBall /> <span>Tênis</span></button>
+                    <button><GiFullMotorcycleHelmet /> <span>Fórmula 1</span></button>
+                </nav>
+            </div>
+
+            <div className="right-section">
+                <div className="search-container">
+                    <IoSearch />
+                    <input type="text" placeholder="Pesquisar no DavansoScore!" />
+                </div>
+
+                <CgProfile className="icon-button" />
+                <FaStar className="icon-button" />
+                <FaQuestionCircle className="icon-button" />
+                <FaCog className="icon-button" />
             </div>
         </header>
     );
